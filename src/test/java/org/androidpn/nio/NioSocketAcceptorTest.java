83b2660eedeadafba3cdd9bfbaf298cbd6c073a9
ChivalrousDig <ChivalrousDig@gmail.com>
2013-10-17 16:23:41 +0800

    修改单元测试包的冲突问题

--- src/test/java/org/androidpn/nio/NioSocketAcceptorTest.java
+++ src/test/java/org/androidpn/nio/NioSocketAcceptorTest.java
@@ -1,9 +1,18 @@
 package org.androidpn.nio;
 
+import java.net.InetSocketAddress;
+
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+import org.junit.Test;
+
+
 public class NioSocketAcceptorTest {
 	
-	
+	@Test
 	public void testNioSocketAcceptor(){
+		NioSocketAcceptor nio = new NioSocketAcceptor();
+		InetSocketAddress isa = new InetSocketAddress(5222);
+		nio.setDefaultLocalAddress(isa);
 		
 	}
 }
